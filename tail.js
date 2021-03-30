dbd1c8555e98c5bc043d73b6560d9e2751b27f1a
Michelle Verge <michellelverge@gmail.com>
2021-03-30 12:47:01 +0000

    return tail of array

---
+++ tail.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(actual) {
+  if (actual.length > 1) {
+    return actual.slice(1);
+  } else {
+    return [];
+  }
+};

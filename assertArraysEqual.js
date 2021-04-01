86fd0f68b499b8af5665cdbc7497d50ea1cfc51b
Michelle Verge <michellelverge@gmail.com>
2021-04-01 01:34:02 +0000

    function to take in two arrays and log approprate message to console

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+  }
+  return true;
+};
+
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+  console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 4], [1, 2, 3]);
+

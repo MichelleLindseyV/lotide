595dd31f2701da530a525b1008ef3040eeaf7d4f
Michelle Verge <michellelverge@gmail.com>
2021-04-02 23:08:30 +0000

    created our own version of map

---
+++ map.js
@@ -0,0 +1,36 @@
+/* Our map function will take in two arguments:
+1. An array to map
+2. A callback function
+The map function will return a new array based on the results
+of the callback function */
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+  }
+  return true;
+};
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+  console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+

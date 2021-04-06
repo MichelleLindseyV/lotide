67c723c45cc3eea4ecffc34061ff7957f17957a3
Michelle Verge <michellelverge@gmail.com>
2021-04-06 02:08:21 +0000

    refactored eqArrays function to 'dry' up code

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 /*Implement a function that takes in two arrays and returns true or false
 based on a perfect match */
@@ -18,11 +11,9 @@ function eqArrays(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-  
+module.exports = eqArrays
+
+
 
 
 
